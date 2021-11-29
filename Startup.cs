4184607d620d9eef6aca17b48be528b9fc6a6da0
tanzumt <89188092+tanzumt@users.noreply.github.com>
2021-11-29 21:57:25 +0900

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
