36f03221e798df7cb07209643bfd7fc286beb478
tanzumt <89188092+tanzumt@users.noreply.github.com>
2021-11-29 20:38:16 +0900

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -40,7 +40,7 @@ namespace aspnet
     <title>Powered By Paketo Buildpacks</title>
   </head>
   <body>
-    <img style=""display: block; margin-left: auto; margin-right: auto; width: 50%;"" src=""/wwwroot/images/paketo-logo-full-color.png""></img>
+    <img style=""display: block; margin-left: auto; margin-right: auto; width: 50%;"" src=~/images/paketo-logo-full-color.png></img>
   </body>
 </html>");
                 });
