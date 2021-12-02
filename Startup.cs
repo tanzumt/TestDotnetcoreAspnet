f382a8ed1a7cd04b7d24fbe50db77359020ffe3f
tanzumt <89188092+tanzumt@users.noreply.github.com>
2021-12-02 16:28:23 +0900

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -37,7 +37,16 @@ namespace aspnet
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync(@"<!DOCTYPE html><html><head><title>Powered By Paketo Buildpacks</title></head><body><img style=""display: block; margin-left: auto; margin-right: auto; width: 50%;"" src=" + ~/images/paketo-logo-full-color.png + "></img></body></html>");
+                    await context.Response.WriteAsync(@"<!DOCTYPE html>
+<html>
+  <head>
+    <title>Powered By Paketo Buildpacks</title>
+  </head>
+  <body>
+    <img style=""display: block; margin-left: auto; margin-right: auto; width: 50%;"" src=""~/images/paketo-logo-full-color.png""></img>
+  </body>
+</html>");
+                });
                 });
             });
         }
